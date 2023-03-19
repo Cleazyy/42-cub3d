f67a77267b332570c0fbf112f1289af373e435ea
Cleazyy <cleazy2002@gmail.com>
2023-03-19 16:16:58 +0100

    protect mlx_init

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 12:28:20 by fluchten          #+#    #+#             */
-/*   Updated: 2023/03/19 16:15:03 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/03/19 16:16:44 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(int ac, char **av)
 	if (!check_args(ac, av))
 		return (1);
 	game.mlx = mlx_init();
+	if (!game->mlx)
+		return (1);
 	game.win = mlx_new_window(game.mlx, 500, 500, "cub3d");
 	mlx_key_hook(game.win, key_pressed, &game);
 	mlx_hook(game.win, 17, 0, close_window, &game);
