8045656d8fafe685cb527c2a0731b6ca775b1485
Cleazyy <cleazy2002@gmail.com>
2023-04-09 16:06:30 +0200

    fix

--- srcs/bonus/minimap_init.c
+++ srcs/bonus/minimap_init.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 14:26:19 by fluchten          #+#    #+#             */
-/*   Updated: 2023/04/09 16:04:48 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/04/09 16:06:12 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ static void	initialize_minimap_table(t_minimap *mini)
 	mini->x = 10;
 	mini->y = 10;
 	mini->w = MINIMAP_WIDTH;
-	if (mini->)
 	mini->h = MINIMAP_HEIGHT;
 	mini->rect = 8;
 	mini->clr_back = 0x1e202a;
