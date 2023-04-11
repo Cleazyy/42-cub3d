e9a5e2805749755b2e8c40432b279ae40fc82eef
Cleazyy <cleazy2002@gmail.com>
2023-04-11 21:27:53 +0200

    fix win_h

--- srcs/graphics/colors.c
+++ srcs/graphics/colors.c
@@ -6,7 +6,7 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 14:44:39 by fluchten          #+#    #+#             */
-/*   Updated: 2023/04/11 21:20:39 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/04/11 21:27:33 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	display_colors(t_data *data)
 		{
 			ft_mlx_pixel_put(&data->img, x, y,
 				data->map.ceiling_rgb);
-			ft_mlx_pixel_put(&data->img, x, y + (WIN_H / 2),
+			ft_mlx_pixel_put(&data->img, x, y + (data->win_h / 2),
 				data->map.floor_rgb);
 			x++;
 		}
