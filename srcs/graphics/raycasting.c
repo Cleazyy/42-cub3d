476296fbb630185574609a3bc7efae7d182361eb
Cleazyy <cleazy2002@gmail.com>
2023-04-22 11:59:13 +0200

    add initialize_ray_table

--- srcs/graphics/raycasting.c
+++ srcs/graphics/raycasting.c
@@ -6,39 +6,11 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 09:27:15 by fluchten          #+#    #+#             */
-/*   Updated: 2023/04/22 11:33:59 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/04/22 11:57:30 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-/* static void	initialize_ray_table(t_ray *ray)
-{
-	ray->dir_x = 0;
-	ray->dir_y = 0;
-	ray->plane_x = 0;
-	ray->plane_y = 0;
-	ray->cam_x = 0;
-	ray->ray_dir_x = 0;
-	ray->ray_dir_y = 0;
-	ray->map_x = 0;
-	ray->map_y = 0;
-	ray->delta_dist_x = 0;
-	ray->delta_dist_y = 0;
-	ray->side_dist_x = 0;
-	ray->side_dist_y = 0;
-	ray->step_x = 0;
-	ray->step_y = 0;
-	ray->side = 0;
-	ray->perpwalldist = 0;
-	ray->start = 0;
-	ray->end = 0;
-	ray->wall_x = 0;
-	ray->tex_x = 0;
-	ray->tex_y = 0;
-	ray->step = 0;
-	ray->tex_pos = 0;
-} */
 
 static int	get_line_height(t_data *data)
 {
--- srcs/parsing/initialization.c
+++ srcs/parsing/initialization.c
@@ -6,17 +6,11 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 10:42:17 by fluchten          #+#    #+#             */
-/*   Updated: 2023/04/22 11:54:41 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/04/22 11:58:58 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static void initialize_data_table(t_data *data)
-{
-	data->win_w = WIN_W;
-	data->win_h = WIN_H;
-}
 
 static void	initialize_map_table(t_data *data)
 {
@@ -42,9 +36,39 @@ static void	initialize_key_table(t_data *data)
 	data->key.arr_r = false;
 }
 
+static void	initialize_ray_table(t_ray *ray)
+{
+	ray->dir_x = 0;
+	ray->dir_y = 0;
+	ray->plane_x = 0;
+	ray->plane_y = 0;
+	ray->cam_x = 0;
+	ray->ray_dir_x = 0;
+	ray->ray_dir_y = 0;
+	ray->map_x = 0;
+	ray->map_y = 0;
+	ray->delta_dist_x = 0;
+	ray->delta_dist_y = 0;
+	ray->side_dist_x = 0;
+	ray->side_dist_y = 0;
+	ray->step_x = 0;
+	ray->step_y = 0;
+	ray->side = 0;
+	ray->perpwalldist = 0;
+	ray->start = 0;
+	ray->end = 0;
+	ray->wall_x = 0;
+	ray->tex_x = 0;
+	ray->tex_y = 0;
+	ray->step = 0;
+	ray->tex_pos = 0;
+}
+
 void	parsing_tables_initialization(t_data *data)
 {
-	initialize_data_table(data);
+	data->win_w = WIN_W;
+	data->win_h = WIN_H;
 	initialize_map_table(data);
 	initialize_key_table(data);
+	initialize_ray_table(&data->ray);
 }
